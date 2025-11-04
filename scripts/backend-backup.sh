f0faeb5c63046b802e02e58d42fcf31e6b6dd0f2
Nabin Maharjan <40271057+Nabin0819@users.noreply.github.com>
2025-11-04 15:39:53 +0545

    Update backend-backup.sh

--- scripts/backend-backup.sh
+++ scripts/backend-backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S") 
-BACKUP_DIR="$HOME/backups/backend" 
+BACKUP_DIR="$HOME/backups/backend"
 mkdir -p "$BACKUP_DIR" 
 tar -czvf "$BACKUP_DIR/backend_backup_$TIMESTAMP.tar.gz" /home/ vagrant/ youtubeclone-backend 
